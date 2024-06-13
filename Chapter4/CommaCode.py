c5aaf6daa446bbecd61a5e44c5b83c317958d065
weisje <20269574+weisje@users.noreply.github.com>
2024-06-13 07:54:11 -0400

    Initial Commit

---
+++ Chapter4/CommaCode.py
@@ -0,0 +1,17 @@
+"""
+CODE REQUIREMENTS:
+Say you have a list value like this:
+spam = ['apples', 'bananas', 'tofu', 'cats']
+Write a function that takes a list value as an argument and returns
+a string with all the items separated by a comma and a space, with and
+inserted before the last item. For example, passing the previous spam list to
+the function would return 'apples, bananas, tofu, and cats'. But your function should be able to work with any list value passed to it. Be sure to test the case where an empty list [] is passed to your function.
+"""
+
+
+def main():
+    print("Initialization")
+
+
+if __name__ == '__main__':
+    main()
