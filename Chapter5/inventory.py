e7a05277c485d4ded859f9845322c7af377aa0d4
weisje <20269574+weisje@users.noreply.github.com>
2024-06-18 09:19:30 -0400

    Updated inventory.py to include function to add items to character inventory for Chapter5 Practice Projects

--- Chapter5/inventory.py
+++ Chapter5/inventory.py
@@ -1,4 +1,5 @@
 """
+CODE ASSIGNMENT 1
 Fantasy Game Inventory
 You are creating a fantasy video game. The data structure to model the
 player’s inventory will be a dictionary where the keys are string values
@@ -14,18 +15,47 @@ Inventory:
 6 torch
 1 dagger
 Total number of items: 62
++++
+
+CODE ASSIGNMENT 2
+Imagine that a vanquished dragon’s loot is represented as a list of strings
+like this:
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+Write a function named addToInventory(inventory, addedItems), where the
+inventory parameter is a dictionary representing the player’s inventory (like
+in the previous project) and the addedItems parameter is a list like dragonLoot.
+The addToInventory() function should return a dictionary that represents the
+updated inventory. Note that the addedItems list can contain multiples of the
+same item.
 """
 
 
-def displayInventory(inventoryList) -> None:
+def addToInventory(currentInventory, addedItems) -> dict:
+    """
+    Function to add items from a list to a dict with the count of number of items included in list
+    :param currentInventory: The items that are in the character inventory before adding items
+    :type currentInventory: dict
+    :param addedItems: Items that are to be added to the character inventory
+    :type addedItems: list
+    :return: dict
+    """
+    for item in addedItems:
+        currentInventory.setdefault(item, 0)
+        currentInventory[item] += 1
+
+    return currentInventory
+
+
+def displayInventory(inventoryDict) -> None:
     """
     Function to take a dictionary with integers as values, display the count of the values with their associated key, and provide a total sum of all the values.
-    :param inventoryList: dict
+    :param inventoryDict: The items that are in the character inventory with their quantity
+    :type inventoryDict: dict
     :return: None
     """
     totalItemSum = 0
     print("Inventory:")
-    for key, value in inventoryList.items():
+    for key, value in inventoryDict.items():
         try:
             totalItemSum += value
             print(f"{value} {key.capitalize()}")
@@ -35,8 +65,12 @@ def displayInventory(inventoryList) -> None:
 
 
 def main():
-    stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 55, 'arrow': 12}
-    displayInventory(stuff)
+    dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+    stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+    updatedInventory = addToInventory(stuff, dragonLoot)
+
+    print()
+    displayInventory(updatedInventory)
 
 
 if __name__ == '__main__':
